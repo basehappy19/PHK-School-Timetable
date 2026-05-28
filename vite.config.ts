e4259d06e9e2ee13822f91070ed8ecb4a7ccb689
Notebook <pakpoom.td@gmail.com>
2026-05-28 07:47:48 +0700

    chore: update vite base path for VPS deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/PHK-School-Timetable/',
+  base: '/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
