ad033b4d86a174c4b75066c7823e7386e527938f
Notebook <pakpoom.td@gmail.com>
2026-05-28 07:34:34 +0700

    Fix: configure base path and GitHub Actions for automatic deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/PHK-School-Timetable/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
